4b30f0534750e242d093b8b993c6e5eb98e13973
Mari test <23mkovtun@mail.ru>
2023-04-14 16:41:03 +0300

    HW1 commit

--- cook_book.py
+++ cook_book.py
@@ -9,8 +9,26 @@ print(full_path)
 with open(full_path, encoding="utf-8") as f:
     res = f.read()
   # print(res)
-    
 
-    
 
-    
+
+cook_book = {}
+key = ['ingredient_name', 'quantity', 'measure']
+with open(full_path, encoding="utf-8") as f:
+    while True:
+        # ingredients.clear()
+        ingredients = []
+        name = f.readline().rstrip()
+        if not name:
+            break
+        ingredient_count = f.readline().rstrip()
+        for i in range(int(ingredient_count)):
+            ing = f.readline().rstrip()
+            ing_list = ing.split("|")
+            ingredient = dict(zip(key, ing_list))
+            ingredient['quantity'] = int(ingredient['quantity'])
+            ingredients.append(ingredient)
+        cook_book[name] = ingredients
+        f.readline().rstrip()
+    # print(cook_book)
+    print(f"Cook_book =", cook_book)
